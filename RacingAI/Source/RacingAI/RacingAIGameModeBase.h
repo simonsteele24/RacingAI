f97cdb360c38e18d63788c77422ccc50ed594d87
Simon Steele <simon.steele@mymail.champlain.edu>
2021-01-07 16:57:54 -0500

    Added original project

---
+++ RacingAI/Source/RacingAI/RacingAIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RacingAIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RACINGAI_API ARacingAIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
